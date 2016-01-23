a175b11a88ed9994f11d31a465ba24e271c66822
Daniel Tomlinson <Dan@Tomlinson.io>
2016-01-23 23:30:53 +0000

    Cleanup

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var foursquareClientID = process.env["FOURSQUARE_CLIENT_ID"];
 var foursquareClientSecret = process.env["FOURSQUARE_CLIENT_SECRET"];
 var foursquareUserToken = process.env["FOURSQUARE_USER_TOKEN"];
 
-var foursquare = require('foursquare')({ "client_id": foursquareClientID, "client_secret": foursquareClientSecret, "v": "20160123" }); var Venue = foursquare.Venue;
+var foursquare = require('foursquare')({ "client_id": foursquareClientID, "client_secret": foursquareClientSecret, "v": "20160123" });
+var Venue = foursquare.Venue;
 var port = process.env.PORT || 3000
 
 var app = express();
@@ -15,27 +16,43 @@ app.use(bodyParser.json());
 app.post('/hook', function (req, res) {
     var body = req.body;
     var type = body.type;
+    var data = body.data;
+
     if (type != "transaction.created") { console.log("Unsupported event type:", type); res.send({}); return; };
+
     var merchant = body.data.merchant;
     var online = merchant.online || false;
-    if (online != false) { console.log("Unsupported transaction: online."); res.send({}); return; };
     var address = merchant.address;
+    var emoji = merchant.emoji;
+
+    var foursquareShout = emoji;
+
+    if (online != false) { console.log("Unsupported transaction: online."); res.send({}); return; };
     
     Venue.find({ radius: 100.0, ll: address.latitude + "," + address.longitude })
         .then(function(body) {
-            var venues = body.response.venues.map(function( attrs ){ return new Venue( attrs ); });
-            venues = venues.filter(function (venue) {
-                return venue.attributes.location.postalCode == address.postcode;
+            var venues = body
+                .response
+                .venues
+                .map(function(attrs) {
+                    return new Venue( attrs );
+                })
+                .filter(function (venue) {
+                    return venue.attributes.location.postalCode == address.postcode;
+                });
+            
+            if (venues.length == 0) { console.log("Error: No matching venues.", res.send({}); return }
+            
+            var venue = venues[0];
+            venue.accessToken(foursquareUserToken)
+                .checkin({ shout: foursquareShout }, function (error, result) {
+                if (error) {
+                    console.log("Error posting to Swarm:", error);
+                }
+                else {
+                    console.log("Posted to Swarm:", merchant.name);
+                }
             });
-
-            var first = venues[0];
-            if (first == null) { console.log("Venue not found"); res.send({}); return;}
-
-            console.log("We have a venue.");
-
-            first.accessToken(foursquareUserToken).checkin({ shout: merchant.emoji }, function (error, result) {
-                console.log("Error:", error, "Checkin:", result.body);
-            })
         });
 
     res.send({});
